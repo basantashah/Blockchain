7d5bdd6f61070c5d0ed7e1fcae7bb4f2bbfdf87b
basantashah <bindassbasanta@hotmail.com>
2019-09-07 21:16:27 +0545

    working for hashing algorithm

--- main.go
+++ main.go
@@ -3,7 +3,12 @@ package main
 import (
 	"bytes"
 	"crypto/sha256"
+	"fmt"
 )
+
+type BlockChain struct {
+	blocks []*Block
+}
 
 type Block struct {
 	Hash     []byte
@@ -23,6 +28,30 @@ func CreateBlock(data string, PrevHash []byte) *Block {
 	return block
 }
 
+func (chain *BlockChain) AddBlock(data string) {
+	prevBlock := chain.blocks[len(chain.blocks)-1]
+	new := CreateBlock(data, prevBlock.Hash)
+	chain.blocks = append(chain.blocks, new)
+}
+
+func Genesis() *Block {
+	return CreateBlock("Genesis", []byte{})
+}
+
+func InitBlockChain() *BlockChain {
+	return &BlockChain{[]*Block{Genesis()}}
+}
+
 func main() {
+	chain := InitBlockChain()
+	chain.AddBlock("First Block after Genesis")
+	chain.AddBlock("Second Block after Genesis")
+	chain.AddBlock("Third Block after Genesis")
+
+	for _, block := range chain.blocks {
+		fmt.Printf("previous Hash : %x\n", block.PrevHash)
+		fmt.Printf("Data in Block: %s\n", block.Hash)
+		fmt.Printf("Hash: %x\n", block.Hash)
+	}
 
 }
